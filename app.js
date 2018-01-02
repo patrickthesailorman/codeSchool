f0b38724c425f31cd777f03a9f17e0599917a451
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-02 15:14:56 +0000

    Add level 2

--- app.js
+++ app.js
@@ -14,3 +14,20 @@ http.createServer(function(request, response) {
 
 }).listen(8080);
 
+
+var http = require('http');
+
+var server = http.createServer();
+  server.on("request", function(request, response) {
+  response.writeHead(200);
+  response.write("Hello, this is dog");
+  response.end();
+});
+   server.on('request', function(request, response) {
+   console.log("New request coming in...");
+}); 
+    server.on('close', function(){
+    console.log("Closing down the server...");
+});
+server.listen(8080);
+
