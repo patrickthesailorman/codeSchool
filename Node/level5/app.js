9539d00aef79a81c778ac1a35370c6de6584a807
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-02 20:22:52 +0000

    Add level 5.4

--- Node/level5/app.js
+++ Node/level5/app.js
@@ -6,6 +6,22 @@
 
 // app.listen(8080);
 
+// var express = require('express');
+// var app = express();
+
+// var quotes = {
+//   'einstein': 'Life is like riding a bicycle. To keep your balance you must keep moving',
+//   'berners-lee': 'The Web does not just connect machines, it connects people',
+//   'crockford': 'The good thing about reinventing the wheel is that you can get a round one',
+//   'hofstadter': 'Which statement seems more true: (1) I have a brain. (2) I am a brain.'
+// };
+
+// app.get('/quotes/:name', function(req,res) {
+//   res.end(quotes[req.params.name]);
+// });
+
+// app.listen(8080);
+
 var express = require('express');
 var app = express();
 
@@ -16,8 +32,12 @@ var quotes = {
   'hofstadter': 'Which statement seems more true: (1) I have a brain. (2) I am a brain.'
 };
 
-app.get('/quotes/:name', function(req,res) {
-  res.end(quotes[req.params.name]);
+app.get('/quotes/:name', function(req, res) {
+  var quote = quotes[req.params.name];
+  res.render('quote.ejs', {
+    name: req.params.name,
+    quote: quote
+  });  
 });
 
 app.listen(8080);
