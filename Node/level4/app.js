41aa94aaf72444c11398df9db451917995b20cfe
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-02 18:56:53 +0000

    Add level 4.4

--- Node/level4/app.js
+++ Node/level4/app.js
@@ -1,5 +1,10 @@
-var http = require('http');
+// var http = require('http');
 
-var myRequest = require('./my_request');
+// var myRequest = require('./my_request');
 
-myRequest('Hello, this is dog.');
+// myRequest('Hello, this is dog.');
+
+var logger = require('./logger');
+
+logger.info('This is some information');
+logger.warn('something bad is happening');
