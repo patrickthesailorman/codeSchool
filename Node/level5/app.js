c897f26f4648a95ac67b001cefa3b8c3ac5cfaa6
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-02 20:15:56 +0000

    Add level 5.3

--- Node/level5/app.js
+++ Node/level5/app.js
@@ -1,7 +1,23 @@
+// var express = require('express');
+// var app = express();
+// app.get('/tweets', function(req, res) {
+//   res.sendFile(__dirname + "/tweets.html");
+// });
+
+// app.listen(8080);
+
 var express = require('express');
 var app = express();
-app.get('/tweets', function(req, res) {
-  res.sendFile(__dirname + "/tweets.html");
+
+var quotes = {
+  'einstein': 'Life is like riding a bicycle. To keep your balance you must keep moving',
+  'berners-lee': 'The Web does not just connect machines, it connects people',
+  'crockford': 'The good thing about reinventing the wheel is that you can get a round one',
+  'hofstadter': 'Which statement seems more true: (1) I have a brain. (2) I am a brain.'
+};
+
+app.get('/quotes/:name', function(req,res) {
+  res.end(quotes[req.params.name]);
 });
 
 app.listen(8080);
