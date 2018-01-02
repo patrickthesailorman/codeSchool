d59af85c5fa38bb7851fd62365aae102d534b2fa
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-02 20:26:17 +0000

    Add level5.5

--- Node/level5/app.js
+++ Node/level5/app.js
@@ -22,22 +22,36 @@
 
 // app.listen(8080);
 
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
 
-var quotes = {
-  'einstein': 'Life is like riding a bicycle. To keep your balance you must keep moving',
-  'berners-lee': 'The Web does not just connect machines, it connects people',
-  'crockford': 'The good thing about reinventing the wheel is that you can get a round one',
-  'hofstadter': 'Which statement seems more true: (1) I have a brain. (2) I am a brain.'
+// var quotes = {
+//   'einstein': 'Life is like riding a bicycle. To keep your balance you must keep moving',
+//   'berners-lee': 'The Web does not just connect machines, it connects people',
+//   'crockford': 'The good thing about reinventing the wheel is that you can get a round one',
+//   'hofstadter': 'Which statement seems more true: (1) I have a brain. (2) I am a brain.'
+// };
+
+// app.get('/quotes/:name', function(req, res) {
+//   var quote = quotes[req.params.name];
+//   res.render('quote.ejs', {
+//     name: req.params.name,
+//     quote: quote
+//   });  
+// });
+
+// app.listen(8080);
+var url = require('url');
+
+options = {
+  // add URL options here
+  protocol: "http:",
+  host: "search.twitter.com",
+  pathname: '/search.json',
+  query: {
+    q: "codeschool"
+  }
 };
 
-app.get('/quotes/:name', function(req, res) {
-  var quote = quotes[req.params.name];
-  res.render('quote.ejs', {
-    name: req.params.name,
-    quote: quote
-  });  
-});
-
-app.listen(8080);
+var searchURL = url.format(options);
+console.log(searchURL);
