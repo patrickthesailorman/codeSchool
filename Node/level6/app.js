64de4ab456bbf687eee7c6c9d0e3296051d331ef
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-03 16:17:58 +0000

    Add level 6.5

--- Node/level6/app.js
+++ Node/level6/app.js
@@ -4,5 +4,10 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 io.on('connection', function(client) {
   console.log("Client connected...");
+  client.on('question', function(question) {
+    client.broadcast.emit('question', question);
+  });
+  
 });
+
 server.listen(8080);
