b3c20c55462d772046b19496b26b2c488e8b5915
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-03 16:41:55 +0000

    Add level 7.4

--- Node/level7/app.js
+++ Node/level7/app.js
@@ -4,3 +4,13 @@ client.set('name', 'todd');
 var result = client.get('question', function(err, data) {
   console.log(data);
 });
+
+var question1 = "Where is the dog?";
+var question2 = "Where is the cat?";
+
+client.lpush("questions", question1, function(err, reply) {
+  console.log(reply);
+});
+client.lpush("questions", question2, function(err, reply) {
+  console.log(reply);
+});
