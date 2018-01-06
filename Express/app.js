d48a439898515e3701bacd25d3ba3f7b4ec424aa
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-06 18:55:29 +0000

    Add level 3.4

--- Express/app.js
+++ Express/app.js
@@ -1,22 +1,23 @@
 var express = require('express');
 var app = express();
 
-var cities = ['Caspiana', 'Indigo', 'Paradise'];
+var cities = {
+  'Lotopia': 'Rough and mountainous',
+  'Caspiana': 'Sky-top island',
+  'Indigo': 'Vibrant and thriving',
+  'Paradise': 'Lush, green plantation',
+  'Flotilla': 'Bustling urban oasis'
+};
 
-app.get('/cities', function(request, response) {
-  if(request.query.search) {
-    response.json(citySearch(request.query.search));
+app.get('/cities/:name', function (request, response) {
+  var cityInfo = cities[request.params.name];
+  if(cityInfo) {
+    response.json(cityInfo);
+  } else {
+    response.status(404).json("City not found");
   }
 });
 
-function citySearch (keyword) {
-  var regexp = RegExp(keyword, 'i');
-  var result = cities.filter(function (city) {
-    return city.match(regexp);
-  });
-
-  return result;
-}
-
 app.listen(3000);
+
 
