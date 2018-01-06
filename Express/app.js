5417f7fa589dc072c4273e90a64b24d40a997403
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-06 18:33:48 +0000

    Add level 2.4

--- Express/app.js
+++ Express/app.js
@@ -1,10 +1,25 @@
+// var express = require('express');
+// var app = express();
+
+// app.get('/cities', function (request, response) {                                                                                                     
+//   var cities = ['Caspiana', 'Indigo', 'Paradise'];
+//   response.send(cities);
+// });
+// app.get('/locations', function(request, response){
+//   response.redirect(301, '/cities');
+// });
+
+
+// app.listen(3001, function () {
+//   console.log("Running Express");
+// });
 var express = require('express');
 var app = express();
 
-app.get('/locations', function (request, response) {
-  var cities = ['Caspiana', 'Indigo', 'Paradise'];
-  response.send(cities);  
+app.use(express.static('public'));
+app.get('/cities', function(req, res){
+  var cities = ['Lotopia', 'Caspiana', 'Indigo'];
+  res.send(cities);
 });
-app.listen(3001, function() {
-  console.log("Running Express");
-});
+
+app.listen(3001);
