e0ce25066d5b1caaf8b165d53953f5ad77c8eefd
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-07 03:25:18 +0000

    add level 3.6

--- Express/app.js
+++ Express/app.js
@@ -9,23 +9,22 @@ var cities = {
   'Flotilla': 'Bustling urban oasis'
 };
 
-app.param('name', function(request, response, next) {
-        request.cityName = parseCityName(request.params.name);
-  next();
- });
-
 app.get('/cities/:name', function (request, response) {
-  var cityInfo = cities[request.cityName];
+  var cityName = parseCityName(request.params.name);
+  var cityInfo = cities[cityName];
+
   if(cityInfo) {
     response.json(cityInfo);
   } else {
-    response.status(404).json("City not found");
+    response.status(404).json('City not found');
   }
 });
 
-function parseCityName(name){
+function parseCityName(name) {
   var parsedName = name[0].toUpperCase() + name.slice(1).toLowerCase();
   return parsedName;
 }
 
-app.listen(3000);                                                                                                                                                                                                                                                                                                            
+app.listen(3000);  
+
+
