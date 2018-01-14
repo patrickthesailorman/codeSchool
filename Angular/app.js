aedb665ce9f802d90bea9b1e9f86605773a7cd54
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-14 17:27:33 +0000

    Add level 4.4

--- Angular/app.js
+++ Angular/app.js
@@ -34,14 +34,27 @@
     };
 
   });
-  
-  app.directive("productDescription", function() {
+
+  app.directive("productDescriptions", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-description.html"
+    };
+  });
+
+  app.directive("productReviews", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-reviews.html"
+    };
+  });
+
+  app.directive("productSpecs", function() {
   return {
-    restrict: 'E',
-    templateUrl: "product-description.html"
+    restrict: "A",
+    templateUrl: "product-specs.html"
   };
 });
-  
 
   var gems = [
     {
@@ -117,7 +130,6 @@
     }
   ];
 })();
-
 
   var gems = [{
       name: 'Azurite',
